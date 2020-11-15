3769695dd6631807d8dc3439e282444a3cf2a39d
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2020-11-15 15:02:18 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # reads the title of all tasks:
-  get 'tasks', to: 'tasks#index'
-  # creates the new form to generate another task
-  get 'tasks/new', to: 'tasks#new'
-  # creates a new task
-  post 'tasks', to: 'tasks#create'
-  # reads the name and description of all tasks
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # getting the form to update a task
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  #patch the form
-  patch 'tasks/:id', to: 'tasks#update'
-  # delete an entry
-  delete 'tasks/:id', to: 'tasks#destroy'
 end
